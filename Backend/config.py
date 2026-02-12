2f0938a0428fcb5237c0859a2ba8b476b45054a3
Guilherme <guilhermevianna63@gmail.com>
2026-02-12 17:47:31 -0300

    feat(storage): integrate Cloudinary and update config files

--- Backend/config.py
+++ Backend/config.py
@@ -6,5 +6,8 @@ load_dotenv()
 
 class Settings(BaseModel):
     database_url: str = os.getenv("DATABASE_URL")
+    cloudinary_api_key: str = os.getenv("CLOUDNARY_API_KEY")
+    cloudinary_api_secret: str = os.getenv("CLOUDNARY_API_SECRET")
+    cloudinary_cloud_name: str = os.getenv("CLOUDNARY_CLOUD_NAME")
 
 settings = Settings()
