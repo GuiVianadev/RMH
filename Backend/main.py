60f1fd5a7f65d060949b50977fdc2a08801f8dfb
Guilherme <guilhermevianna63@gmail.com>
2026-02-12 22:51:38 -0300

    feat(api): add comment router

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from documents.routes import router as document_router
+from comments.routes import router as comment_router
 
 app = FastAPI(title="RMH Backend API")
 app.add_middleware(
@@ -12,6 +13,7 @@ app.add_middleware(
 )
 
 app.include_router(document_router)
+app.include_router(comment_router)
 
 @app.get("/health")
 def health_check():
